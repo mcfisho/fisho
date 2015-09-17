c151d726195212b83e343cbf361baf3e3795c971
Mc Fisho <mcfisho@gmail.com>
2015-09-17 17:35:58 +0700

    Git 3nd

--- TESTRun/src/com/mc/run/TeSTRun.java
+++ TESTRun/src/com/mc/run/TeSTRun.java
@@ -4,8 +4,9 @@ public class TeSTRun {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello");
-		System.out.print("Go");
+		System.out.println("Hello");
+		System.out.println("Go");
+		System.out.println("Na Ja ...");
 	}
 
 }
